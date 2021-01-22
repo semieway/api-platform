fd5a576415c0d67dc3e00ba90dc48b3c3c2a4bd0
Alex Sirotin <semieway@gmail.com>
2021-01-22 18:58:53 +0500

    Create set text description method & add createAt automatically

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -53,6 +53,11 @@ class CheeseListing
      */
     private $isPublished;
 
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -75,9 +80,9 @@ class CheeseListing
         return $this->description;
     }
 
-    public function setDescription(string $description): self
+    public function setTextDescription(string $description): self
     {
-        $this->description = $description;
+        $this->description = nl2br($description);
 
         return $this;
     }
@@ -97,13 +102,6 @@ class CheeseListing
     public function getCreatedAt(): ?\DateTimeInterface
     {
         return $this->createdAt;
-    }
-
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
     }
 
     public function getIsPublished(): ?bool
