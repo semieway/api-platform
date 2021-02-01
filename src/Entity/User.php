ff82d75d53dec79b19379c1bdf21401de9e865d6
Alex Sirotin <semieway@gmail.com>
2021-02-01 19:21:39 +0500

    Add cheeseListing property to read group

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -59,6 +59,7 @@ class User implements UserInterface
 
     /**
      * @ORM\OneToMany(targetEntity=CheeseListing::class, mappedBy="owner")
+     * @Groups("user:read")
      */
     private $cheeseListings;
 
