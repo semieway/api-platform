4c822786222ff882f59862df987dc2249b68f8b0
Alex Sirotin <semieway@gmail.com>
2021-02-02 13:00:53 +0500

    Embed cheese entity fields

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -49,7 +49,7 @@ class CheeseListing
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"cheese_listing:read", "cheese_listing:write", "user:read"})
+     * @Groups({"cheese_listing:read", "cheese_listing:write", "user:read", "user:write"})
      * @Assert\NotBlank()
      * @Assert\Length(
      *     min=2,
@@ -70,7 +70,7 @@ class CheeseListing
      * The price of this delicious cheese, in cents.
      *
      * @ORM\Column(type="integer")
-     * @Groups({"cheese_listing:read", "cheese_listing:write", "user:read"})
+     * @Groups({"cheese_listing:read", "cheese_listing:write", "user:read", "user:write"})
      * @Assert\NotBlank()
      */
     private $price;
@@ -127,7 +127,7 @@ class CheeseListing
     }
 
     /**
-     * @Groups({"cheese_listing:write"})
+     * @Groups({"cheese_listing:write", "user:write"})
      * @SerializedName("description")
      */
     public function setTextDescription(string $description): self
